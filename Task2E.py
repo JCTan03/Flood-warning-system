407ce1a007eb24e16f3a8c76dde238071ccbe2c1
mk2158-cam <mk2158@cam.ac.uk>
2023-01-24 18:10:17 +0000

    Completed Task2E

--- Task2E.py
+++ Task2E.py
@@ -1,10 +1,26 @@
-from floodsystem.flood import stations_level_over_threshold
+import datetime
+from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.plot import plot_water_levels
 
 def run():
     """Requirements for Task 2E"""
 
-    # to do after Task 2C completed
+    # Build list of stations
+    stations = build_station_list()
+
+    # update live water levels
+    update_water_levels(stations)
+
+    # get first 5 stations with highest relative water level
+    stations_first_5_rel_water_level = stations_highest_rel_level(stations, 5)
+
+    # plot each station's last 10 days water levels
+    for station in stations_first_5_rel_water_level:
+        dates, levels = fetch_measure_levels(station.measure_id,
+                                     dt=datetime.timedelta(days=10))
+        plot_water_levels(station, dates, levels)
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
