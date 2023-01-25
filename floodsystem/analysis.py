c8a7ce24145626df7956bf57d7105d33796a2e58
mk2158-cam <mk2158@cam.ac.uk>
2023-01-25 11:20:36 +0000

    Task2F, created polyfit function

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 """This module provides functionality for analysing level data
 """
 
@@ -9,3 +13,19 @@ def polyfit(dates, levels, p):
     Return:
      tuple of (i) the polynomial object and (ii) any shift of the time (date) axis
     """
+
+    # Convert list of dates to list of floats, where the floats are the time in days (including fractions of days) since the year 0001
+    x = matplotlib.dates.date2num(dates)
+
+    # Shift dates
+    shift = dates[0]
+    x_shift = x[0]
+    x -= x_shift
+
+    # Find coefficients of best-fit polynomial f(x) of degree p, using x shifted values
+    p_coeff = np.polyfit(x, levels, p)
+
+    # Convert coefficient into a polynomial that can be evaluated,
+    poly = np.poly1d(p_coeff)
+
+    return (poly, shift)
