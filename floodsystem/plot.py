3ea336b6271d5e8dcb8e7a94b225d43ed0191134
mk2158-cam <mk2158@cam.ac.uk>
2023-01-25 11:28:02 +0000

    Task2F plot_water_level_with_fit function created

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from .analysis import polyfit
+
 
 """This module contains a collection of functions related to
 plotting water level data.
@@ -33,4 +35,32 @@ def plot_water_levels(station, dates, levels):
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
+    plt.show()
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    """function that plots the water level data and the best-fit polynomial"""
+
+    # Plot water level data
+    plt.plot(dates, levels)
+    
+    # Get best-fit polynomial
+    poly, d0 = polyfit(dates, levels, p)
+
+    # Plot polynomial on same graph
+    plt.plot(dates, poly(dates-d0))
+
+    # Add low/high water level marks
+    lows = [station.typical_range[0]]*len(dates)
+    highs = [station.typical_range[1]]*len(dates)
+
+    plt.plot(dates, lows)
+    plt.plot(dates, highs)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+
+    # Display plot
     plt.show()
